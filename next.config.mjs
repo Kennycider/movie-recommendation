fb7c80963512f9580a74178a0d7a7c0fff10a407
Aizen1d <ddreataza@gmail.com>
2024-08-29 21:15:50 +0800

    allow images outside

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    scrollRestoration: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],      
+  },
+};
 
 export default nextConfig;
